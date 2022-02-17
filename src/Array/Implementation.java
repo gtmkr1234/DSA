5c3f05e956e670bfa3b6d44ab30e8046a960b87e
pc <gtmkr1234@gmail.com>
2022-02-17 22:51:49 +0530

    Implementation of various operations on Array data structure

--- src/Array/Implementation.java
+++ src/Array/Implementation.java
@@ -1,4 +1,29 @@
 package Array;
 
+import java.util.Scanner;
+
+class ArrayImplementation{
+    private int [] arr;
+
+    public ArrayImplementation() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of your array");
+        int n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.MIN_VALUE;
+        }
+    }
+    public void traverse(){
+        for(int res : arr){
+            System.out.println(res);
+        }
+    }
+}
 public class Implementation {
+    public static void main(String[] args) {
+        ArrayImplementation obj = new ArrayImplementation();
+        obj.traverse();
+    }
+
 }
