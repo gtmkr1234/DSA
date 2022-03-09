3b7229e580d023e6c08ecd10ff04823e184b1b2e
pc <gtmkr1234@gmail.com>
2022-03-09 11:32:34 +0530

    test

--- src/tests/test1.java
+++ src/tests/test1.java
@@ -1,66 +1,90 @@
 package tests;
-import java.util.PriorityQueue;
-
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Scanner;
 
 // Question -
 /*             an array is given in which we have to add the longest subsequence in a priority queue
 */
 public class test1 {
-    int n;
-    int [] arr;
-    public test1(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the number of elements: ");
-        n = s.nextInt();
-        arr = new int[n];
-        System.out.println("Enter the " + n + " elements of the array");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = s.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue enq = new PriorityQueue();
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
         }
-    }
-    public boolean isEmpty(int arr[]){
-        int counter = 0;
-        for (int i = 0; i < arr.length; i++) {
-            counter ++;
-        }
-        if(counter == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    public boolean oddCounter(int arr[]){
-        int counter =0;
-        for(int i: arr){
-            if(i%2!=0){
-                counter ++;
+
+        int sum,len, j = 0,flag=0,temp=0;
+        for(int i=0;i<n;i++)
+        {
+            flag=0;
+            if(a[i]%2!=0)
+            {
+                flag=1;
+                temp=1;
+                len=0;sum=0;
+                for(j=i;j<n && a[j]%2!=0;j++)
+                {
+                    len++;
+                    sum+=a[j];
+                }
+                enq.enque(len,sum);
             }
+
+            if(flag==1)
+                i=j;
         }
-        if(counter == 0){
-            return true;
+
+
+        if(temp==0)
+        {
+            System.out.println("can't perform the operation");
         }
-        else{
-            return false;
-        }
+        else
+            enq.display();
     }
 }
-class exe{
-    public static void main(String[] args) {
-        test1 obj = new test1();
-        if(obj.isEmpty(obj.arr)){
-            System.out.println("empty array");
+class node{
+    int length;
+    int priority;
+    node next;
+    public node(int length, int priority){
+        this.length = length;
+        this.priority = priority;
+        next = null;
+    }
+}
+class PriorityQueue{
+    node rear;
+    node front;
+
+    PriorityQueue(){
+        rear = null;
+        front = null;
+    }
+    public void enque(int len, int sum){
+        node newnode = new node(len,sum);
+        if(front == null|| newnode.priority < front.priority){
+            newnode.next = front;
+            front = newnode;
         }
         else{
-            if(obj.oddCounter(obj.arr)){
-                System.out.println("Can not perform the operation!!!");
+            node temp = front;
+            while(temp.next == null && temp.next.priority <= newnode.next.priority){
+                temp = temp.next;
             }
-            else{
-                PriorityQueue <Integer> obj1 = new PriorityQueue<>();
-            }
+            newnode.next = temp.next;
+            temp.next = newnode;
+        }
+
+    }
+    public void display(){
+        node tmp = front;
+        while(tmp.next != null){
+            System.out.println( tmp.next + " " + tmp.priority);
+            tmp = tmp.next;
         }
     }
 }
