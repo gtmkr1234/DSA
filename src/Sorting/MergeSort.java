cbcc970a38ffc6f6129e15d0e55f6fec7716c167
pc <gtmkr1234@gmail.com>
2022-03-26 11:23:58 +0530

    sorting algorithms

--- src/Sorting/MergeSort.java
+++ src/Sorting/MergeSort.java
@@ -1,52 +1,25 @@
 package Sorting;
 
-public class MergeSort {
-    public static void mergeSort(int [] arr, int l, int r){
-        if(l<r){
-            int mid = (l+r)/2;
-            mergeSort(arr,l,mid);
-            mergeSort(arr,mid+1,r);
-            merge(arr, l, mid, r);
-        }
-    }
-    public static void merge(int [] arr, int l, int mid, int r){
-        int i = l;
-        int j = mid+1;
-        int k = l;
-        int [] b = new int[arr.length] ;
-        while(i<=mid && j<=r){
-            if(arr[i]<arr[j]){
-                b[k] = arr[i];
-                i++;
-                k++;
-            }
-            else{
-                b[k] = arr[j];
-                j++;
-                k++;
-            }
+import java.util.Arrays;
 
-        }
-        if(i>mid){
-            while(j<=r){
-                b[k++] = arr[j++];
-            }
-        }
-        else{
-            while(i<=mid){
-                b[k++] = arr[i++];
-            }
-        }
-        for(k=l; k<r; k++){
-            arr[k] = b[k];
-        }
-    }
-
+public class MergeSort{
     public static void main(String[] args) {
-        int[] arr = {15,45,1,56,78,78,89,65,124,2};
-        mergeSort(arr,1,9);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+
+    }
+    static int [] mergeSort(int [] arr){
+        if(arr.length == 1){
+            return arr;
         }
+        else {
+            int mid = arr.length / 2;
+            int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+            int[] right = mergeSort(Arrays.copyOfRange(arr, mid + 1, arr.length));
+            return merge(left, right);
+        }
+    }
+    private static int [] merge(int[]first, int[] last){
+        int [] res = new int[first.length+last.length];
+        int i,j,k = 0;
+
     }
 }
