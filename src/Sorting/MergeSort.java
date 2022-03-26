3bd64cd263948f0782100c461412ee4b13297287
pc <gtmkr1234@gmail.com>
2022-03-26 12:00:44 +0530

    sorting algorithms

--- src/Sorting/MergeSort.java
+++ src/Sorting/MergeSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MergeSort{
     public static void main(String[] args) {
-        int [] arr = {56,5,479,54,10,0};
+        int [] arr = {5,4,3,2,1,0};
         arr = mergeSort(arr);
         System.out.println(Arrays.toString(arr));
     }
---
+++ src/Sorting/MergeSortInPlace.java
@@ -0,0 +1,10 @@
+package Sorting;
+
+public class MergeSortInPlace {
+    public static void main(String[] args) {
+
+    }
+    public static void mergeSort(int[] arr, int start, int end){
+
+    }
+}
