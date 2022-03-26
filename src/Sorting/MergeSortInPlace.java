0d24be77fe2f9b5d2f06db476753d2580b13cce5
pc <gtmkr1234@gmail.com>
2022-03-26 13:07:36 +0530

    sorting algorithms

--- src/Sorting/MergeSortInPlace.java
+++ src/Sorting/MergeSortInPlace.java
@@ -4,53 +4,51 @@ import java.util.Arrays;
 
 public class MergeSortInPlace {
     public static void main(String[] args) {
-            int [] a= {5,4,3,2,1};
-            mergeSort(a,0,a.length);
-            System.out.println(Arrays.toString(a));
-
+        int [] arr = {5,4,3,2,1,0};
+        mergeSortinplace(arr,0,arr.length);
+        System.out.println(Arrays.toString(arr));
     }
-    public static void mergeSort(int[] arr, int start, int end){
-            if(end-start==1){
-                return;
-            }
-            else{
-                int mid = (start+end)/2;
-                mergeSort(arr,start,mid);
-                mergeSort(arr,mid,end);
-                mergeInPlace(arr,start,mid,end);
-            }
+    static void mergeSortinplace(int [] arr, int s, int e){
+        if((e-s) == 1){
+            return ;
+        }
+        else {
+            int mid = (s+e) / 2;
+            mergeSortinplace(arr,s,mid);
+            mergeSortinplace(arr,mid,e);
+            mergeinplace(arr,s,mid,e);
+        }
     }
-    private static void mergeInPlace(int [] arr, int s, int m, int e){
+    private static void mergeinplace(int[]arr, int s,int m,int e){
         int [] res = new int[e-s];
-            int i = s;
-            int j = m;
-            int k = 0;
+        int i = s;
+        int j=m;
+        int k=0;
 
-            while(i<m && j<e){
-                if(arr[i]<arr[j]){
-                    res[k] = arr[i];
-                    i++;
-                }
-                else{
-                    res[k] = arr[j];
-                    j++;
-                }
-                k++;
-            }
-            while(i<m){
+        while(i<m && j<e){
+            if(arr[i]< arr[j]){
                 res[k] = arr[i];
                 i++;
-                k++;
             }
-            while(j < e){
+            else{
                 res[k] = arr[j];
-                k++;
                 j++;
             }
-            for(int l = 0; j<res.length; l++){
-                arr[s+l] = res[l];
-            }
-
+            k++;
+        }
+        while(i<m){
+            res[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j<e){
+            res[k] = arr[j];
+            j++;
+            k++;
+        }
+        for(int l=0;l<res.length;l++){
+            arr[s+l] = res[l];
+        }
     }
 
 }
