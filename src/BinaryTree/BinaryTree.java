52d082a59d3ca916c583133502e49d5dd12a0317
pc <gtmkr1234@gmail.com>
2022-03-28 11:17:50 +0530

    Binary Tree Representation

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -3,13 +3,33 @@ class BinaryNode<T>{
     T data;
     BinaryNode<T> left;
     BinaryNode<T> right;
-    public void BinaryNode(T data){
+    public BinaryNode(T data){
         this.data = data;
     }
 }
 public class BinaryTree {
+    public static void print(BinaryNode root){
+        if (root == null){
+            return;
+        }
+        else{
+            System.out.println(root.data);
+            print(root.left);
+            print(root.right);
+        }
+    }
     public static void main(String[] args) {
         BinaryNode <Integer> root = new BinaryNode<Integer>(15);
+        BinaryNode<Integer> Rleft = new BinaryNode<Integer>(10);
+        BinaryNode<Integer> Rright = new BinaryNode<Integer>(20);
+        BinaryNode<Integer> o1 = new BinaryNode<Integer>(55);
+        BinaryNode<Integer> o2 = new BinaryNode<Integer>(45);
+
+        root.left=Rleft;
+        root.right=Rright;
+        Rright.right = o1;
+        Rleft.left = o2;
+        print(root);
 
     }
 }
