0097a83cfe19d64517972ff6e06e67f133850b20
pc <gtmkr1234@gmail.com>
2022-04-18 10:38:43 +0530

    implementation of graph DS by using Hashmap

---
+++ src/Graph/test.java
@@ -0,0 +1,11 @@
+package Graph;
+import java.util.HashMap;
+
+public class test {
+    public static void main(String[] args) {
+        HashMap <String,Integer> obj = new HashMap<>();
+        obj.put("ABC",50);
+        obj.put("ABC", 56);
+        obj.get("ABC");
+    }
+}
