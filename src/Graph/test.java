b4860b720d687d1529bb78cb7fa1186b982c3fa1
pc <gtmkr1234@gmail.com>
2022-04-18 10:52:33 +0530

    implementation of graph DS by using Hashmap

--- src/Graph/test.java
+++ src/Graph/test.java
@@ -1,16 +1,18 @@
 package Graph;
 import java.util.HashMap;
+import java.util.Map;
 
 public class test {
     public static void main(String[] args) {
         HashMap <String,Integer> obj = new HashMap<>();
         obj.put("ABC",50);
         obj.put("DEF", 56);
-        int a = obj.get("ABC");
-        System.out.println(a);
-        int b = obj.getOrDefault("DEF",-1);
-        System.out.println(b);
-        System.out.println(obj);
+        obj.put("IJK",45);
+        for(Map.Entry<String,Integer> res : obj.entrySet()){
+            System.out.println(res.getKey());
+            System.out.println(res.getValue());
+            System.out.println(res.getClass());
+        }
         //Key can not be duplicated but values can be
     }
 }
