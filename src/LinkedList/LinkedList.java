37242242b157e6fea10e002de4af7c993f4d2e19
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-26 15:09:06 +0530

    Bst

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,15 @@
+package LinkedList;
+public class LinkedList {
+    ListNode head = new ListNode();
+    private static class ListNode{
+        int data;
+        ListNode next;
+        ListNode(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
